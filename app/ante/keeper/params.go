1e32f871b95603a1c436635200bd99b4967918a6
Yun <sw.yunsuk@gmail.com>
2022-06-17 14:57:08 +0900

    fix comment

--- app/ante/keeper/params.go
+++ app/ante/keeper/params.go
@@ -6,14 +6,13 @@ import (
 	"github.com/terra-money/core/v2/app/ante/types"
 )
 
-// HistoricalEntries = number of historical info entries
-// to persist in store
+// MinimumCommissionEnforced - the flag represents whether minimum commission enforced or not
 func (k Keeper) MinimumCommissionEnforced(ctx sdk.Context) (res bool) {
 	k.paramstore.Get(ctx, types.ParamStoreKeyMinimumCommissionEnforced, &res)
 	return
 }
 
-// BondDenom - Bondable coin denomination
+// MinimumCommission - Minimum Commission enforced for all validators
 func (k Keeper) MinimumCommission(ctx sdk.Context) (res sdk.Dec) {
 	k.paramstore.Get(ctx, types.ParamStoreKeyMinimumCommission, &res)
 	return
