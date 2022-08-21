12e613055fbe96ef6e2d7804e2d437dfafccac63
kevinnwi <40870001+kevinnwi@users.noreply.github.com>
2022-08-21 21:52:53 +0700

    Update config.go

--- cmd/terrad/config.go
+++ cmd/terrad/config.go
@@ -32,7 +32,7 @@ func initAppConfig() (string, interface{}) {
 	//   own app.toml to override, or use this default value.
 	//
 	// In simapp, we set the min gas prices to 0.
-	srvCfg.MinGasPrices = "0uluna"
+	srvCfg.MinGasPrices = "0 uluna"
 
 	terraAppConfig := TerraAppConfig{
 		Config:     *srvCfg,
