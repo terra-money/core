a5056d6b814328f7e822c79274b72ddc1c19f903
emidev98 <49301655+emidev98@users.noreply.github.com>
2023-03-21 08:22:50 +0100

    Revert "Cleanup (#98)"
    This reverts commit a97277b3b51defb8a54e580cfaaf2608d2ae99fe.

--- app/ante/sigverify.go
+++ app/ante/sigverify.go
@@ -18,6 +18,7 @@ var (
 	// simulation signature values used to estimate gas consumption
 	key                = make([]byte, secp256k1.PubKeySize)
 	simSecp256k1Pubkey = &secp256k1.PubKey{Key: key}
+	simSecp256k1Sig    [64]byte
 
 	_ authsigning.SigVerifiableTx = (*legacytx.StdTx)(nil) // assert StdTx implements SigVerifiableTx
 )
