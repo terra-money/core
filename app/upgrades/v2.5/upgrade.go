6372b59f1a210e793f5e16b067007ed8512b53f1
javiersuweijie <javier.su.weijie@gmail.com>
2023-09-04 15:39:56 +0800

    fix: added ica controller params

--- app/upgrades/v2.5/upgrade.go
+++ app/upgrades/v2.5/upgrade.go
@@ -9,6 +9,8 @@ import (
 	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	icacontrollerkeeper "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/controller/keeper"
+	icacontrollertypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/controller/types"
 	clientkeeper "github.com/cosmos/ibc-go/v7/modules/core/02-client/keeper"
 	ibcexported "github.com/cosmos/ibc-go/v7/modules/core/exported"
 	ibctmmigrations "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint/migrations"
@@ -21,6 +23,7 @@ func CreateUpgradeHandler(
 	clientKeeper clientkeeper.Keeper,
 	paramsKeeper paramskeeper.Keeper,
 	consensusParamsKeeper consensuskeeper.Keeper,
+	icacontrollerKeeper icacontrollerkeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		// READ: https://github.com/cosmos/cosmos-sdk/blob/v0.47.4/UPGRADING.md#xconsensus
@@ -38,6 +41,9 @@ func CreateUpgradeHandler(
 		params := clientKeeper.GetParams(ctx)
 		params.AllowedClients = append(params.AllowedClients, ibcexported.Localhost)
 		clientKeeper.SetParams(ctx, params)
+
+		// READ: https://github.com/terra-money/core/issues/166
+		icacontrollerKeeper.SetParams(ctx, icacontrollertypes.DefaultParams())
 
 		return mm.RunMigrations(ctx, cfg, fromVM)
 	}
