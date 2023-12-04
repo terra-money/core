efe3fd89bb805b6a2dbf56f9d58d60031169914e
emidev98 <emilian@terra.money>
2023-12-04 19:15:39 +0200

    feat(v2.9): automatically install relayer when not installed

--- integration-tests/src/teardown.ts
+++ integration-tests/src/teardown.ts
@@ -4,7 +4,7 @@ import path from 'path';
 module.exports = async () => {
     try {
         const pathToDataDir = path.join(__dirname, "/test-data");
-        execSync("pkill rly")
+        execSync("pkill relayer")
         execSync("pkill terrad")
         execSync("pkill terrad")
         execSync(`rm -r ${pathToDataDir}`)
