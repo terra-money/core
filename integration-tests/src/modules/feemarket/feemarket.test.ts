4abf9566b154e39e23f08d08a697ae23b9ab334b
freeelancer <lmengsplist96@gmail.com>
2024-01-24 20:50:11 +0800

    update feemarket.test

--- integration-tests/src/modules/feemarket/feemarket.test.ts
+++ integration-tests/src/modules/feemarket/feemarket.test.ts
@@ -1,8 +1,9 @@
 import { Coins, MsgSubmitProposal, MsgVote } from "@terra-money/feather.js";
-import { MsgParams } from "@terra-money/feather.js/dist/core/feemarket/msgs";
 import { Params } from "@terra-money/feather.js/dist/core/feemarket/params";
+import { State } from "@terra-money/feather.js/dist/core/feemarket/state";
 import { VoteOption } from "@terra-money/terra.proto/cosmos/gov/v1beta1/gov";
 import { blockInclusion, getLCDClient, getMnemonics, votingPeriod } from "../../helpers";
+import { MsgParams, MsgState} from "@terra-money/feather.js/dist/core/feemarket/proposals";
 
 describe("Feemarket Module (https://github.com/terra-money/feemarket/tree/v0.0.1-alpha.2-terra.0) ", () => {
     // Prepare environment clients, accounts and wallets
@@ -26,8 +27,9 @@ describe("Feemarket Module (https://github.com/terra-money/feemarket/tree/v0.0.1
                         '30000000',
                         '1',
                         true,
+                        'uluna',
                     ),
-                    'asd'
+                    'gov', // TODO: change to module address of 'gov'
                     )],
                 Coins.fromString("1000000000uluna"),
                 val2WalletAddress,
@@ -40,7 +42,10 @@ describe("Feemarket Module (https://github.com/terra-money/feemarket/tree/v0.0.1
                 msgs: [msgProposal],
                 chainID: "test-2",
             });
+            console.log("adhjkgasjkghajkshg")
             let result = await LCD.chain2.tx.broadcastSync(tx, "test-2");
+            console.log("asgasga")
+
             await blockInclusion();
 
             // Check that the proposal was created successfully
@@ -66,7 +71,68 @@ describe("Feemarket Module (https://github.com/terra-money/feemarket/tree/v0.0.1
             expect(txResult.code).toBe(0);
         }
         catch (e: any) {
-            console.log(e);
+            console.log(e.response.data);
+        }
+
+        // Query the feemarket params and validate the new values
+    });
+
+
+    test.only('Must create a new global eip1559 fees state for uluna', async () => {
+        try {
+            const msgProposal = new MsgSubmitProposal(
+                [new MsgState(
+                    new State(
+                        'uluna',
+                        '0.0015',
+                        '0.0015',
+                        '0.125',
+                        [],
+                        '0'
+                    ),
+                    'gov', // TODO: change to module address of 'gov'
+                    )],
+                Coins.fromString("1000000000uluna"),
+                val2WalletAddress,
+                "metadata",
+                "title",
+                "summary"
+            );
+            // Create an alliance proposal sign and submit on chain-1
+            let tx = await val2Wallet.createAndSignTx({
+                msgs: [msgProposal],
+                chainID: "test-2",
+            });
+            console.log("adhjkgasjkghajkshg")
+            let result = await LCD.chain2.tx.broadcastSync(tx, "test-2");
+            console.log("asgasga")
+
+            await blockInclusion();
+
+            // Check that the proposal was created successfully
+            let txResult = await LCD.chain2.tx.txInfo(result.txhash, "test-2") as any;
+            expect(txResult.code).toBe(0);
+
+            // Get the proposal id and validate exists
+            let proposalId = Number(txResult.logs[0].eventsByType.submit_proposal.proposal_id[0]);
+            expect(proposalId)
+
+            // Vote for the proposal
+            tx = await val2Wallet.createAndSignTx({
+                msgs: [new MsgVote(
+                    proposalId,
+                    val2WalletAddress,
+                    VoteOption.VOTE_OPTION_YES
+                )],
+                chainID: "test-2",
+            });
+            result = await LCD.chain1.tx.broadcastSync(tx, "test-2");
+            await votingPeriod();
+            txResult = await LCD.chain1.tx.txInfo(result.txhash, "test-2")
+            expect(txResult.code).toBe(0);
+        }
+        catch (e: any) {
+            console.log(e.response.data);
         }
 
         // Query the feemarket params and validate the new values
