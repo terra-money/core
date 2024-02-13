2e7671ef22852bdbcae80ed888b7f366b033916e
emidev98 <emilian@terra.money>
2024-02-13 18:41:41 +0200

    wip: feemarket tests

--- integration-tests/src/modules/feemarket/feemarket.test.ts
+++ integration-tests/src/modules/feemarket/feemarket.test.ts
@@ -1,9 +1,10 @@
-import { Coins, MsgSend, MsgSubmitProposal, MsgVote } from "@terra-money/feather.js";
-import { Params } from "@terra-money/feather.js/dist/core/feemarket/params";
-import { MsgFeeDenomParam, MsgParams } from "@terra-money/feather.js/dist/core/feemarket/proposals";
+import { Coins, Fee, MnemonicKey, MsgSend, MsgSubmitProposal, MsgVote } from "@terra-money/feather.js";
 import { VoteOption } from "@terra-money/terra.proto/cosmos/gov/v1beta1/gov";
-import BigNumber from 'bignumber.js';
 import { blockInclusion, getLCDClient, getMnemonics, votingPeriod } from "../../helpers";
+import { FeemarketParams, MsgFeeDenomParam, MsgParams } from "@terra-money/feather.js/dist/core/feemarket";
+import { execSync, exec } from 'child_process';
+import path from 'path';
+
 
 describe("Feemarket Module (https://github.com/terra-money/feemarket/tree/v0.0.1-alpha.2-terra.0) ", () => {
     // Prepare environment clients, accounts and wallets
@@ -11,42 +12,49 @@ describe("Feemarket Module (https://github.com/terra-money/feemarket/tree/v0.0.1
     const accounts = getMnemonics();
     const val1Wallet = LCD.chain1.wallet(accounts.val1);
     const val1WalletAddress = val1Wallet.key.accAddress("terra");
-    const rly1Wallet = LCD.chain1.wallet(accounts.rly1);
-    const rly1WalletAddress = rly1Wallet.key.accAddress("terra");
 
-    test('Must create a new global eip1559 fees param + test dynamic fees', async () => {
+    // Stop the relayer that way it does not affect the 
+    // feemarket due the high amount of transactions
+    beforeAll(() => {
         try {
-            const params = new Params(
-                '0',    
-                '1000000000000000000',
-                '0',
-                '135000000000000000',
-                '135000000000000000',
-                '5000',
-                '30000000',
-                '1',
-                true,
-                'uluna',
-            )
-            const msgProposal = new MsgSubmitProposal(
-                [new MsgParams(
-                    params,
-                    'terra10d07y265gmmuvt4z0w9aw880jnsr700juxf95n',
-                )],
-                Coins.fromString("1000000000uluna"),
-                val1WalletAddress,
-                "metadata",
-                "title",
-                "summary"
-            );
+            execSync("pkill relayer")
+        }
+        catch (e) {
+            console.log(e)
+        }
+    });
 
-            // Create an update params proposal sign and submit on chain-1
+
+    test('Must send a proposal to setup new global eip1559 fees param', async () => {
+        try {
+            // Create a FeemarketParams proposal change and 
+            // submit it on chain 1.
             let tx = await val1Wallet.createAndSignTx({
-                msgs: [msgProposal],
+                msgs: [new MsgSubmitProposal(
+                    [new MsgParams(
+                        FeemarketParams.fromData({
+                            alpha: '0',
+                            beta: '1000000000000000000',
+                            theta: '0',
+                            min_learning_rate: '135000000000000000',
+                            max_learning_rate: '135000000000000000',
+                            target_block_utilization: '5000',
+                            max_block_utilization: '30000000',
+                            window: '1',
+                            enabled: true,
+                            default_fee_denom: 'uluna'
+                        }),
+                        'terra10d07y265gmmuvt4z0w9aw880jnsr700juxf95n',
+                    )],
+                    Coins.fromString("1000000000uluna"),
+                    val1WalletAddress,
+                    "metadata",
+                    "title",
+                    "summary"
+                )],
                 chainID: "test-1",
             });
             let result = await LCD.chain1.tx.broadcastSync(tx, "test-1");
-
             await blockInclusion();
 
             // Check that the proposal was created successfully
@@ -54,8 +62,8 @@ describe("Feemarket Module (https://github.com/terra-money/feemarket/tree/v0.0.1
             expect(txResult.code).toBe(0);
 
             // Get the proposal id and validate exists
-            let proposalId = Number(txResult.logs[0].eventsByType.submit_proposal.proposal_id[0]);
-            expect(proposalId)
+            const proposalId = Number(txResult.logs[0].eventsByType.submit_proposal.proposal_id[0]);
+            expect(proposalId).toBeDefined();
 
             // Vote for the proposal
             tx = await val1Wallet.createAndSignTx({
@@ -72,96 +80,108 @@ describe("Feemarket Module (https://github.com/terra-money/feemarket/tree/v0.0.1
             expect(txResult.code).toBe(0);
 
             // Query the feemarket params and validate the new values
-            let foundParams = await LCD.chain1.feemarket.params("test-1") as any;
-            checkParams(foundParams.params, params)
-            
-            // Start fee tests
-            for (let i=0;i<4;i++) {
-                let sendTx = await val1Wallet.createAndSignTx({
-                    msgs: [
-                        new MsgSend(
-                            val1WalletAddress,
-                            rly1WalletAddress,
-                            Coins.fromString("1uluna"),
-                        ),
-                    ],
-                    chainID: "test-1",
+            let foundParams = await LCD.chain1.feemarket.params("test-1");
+            expect(foundParams.toData())
+                .toStrictEqual({
+                    "alpha": "0",
+                    "beta": "1",
+                    "theta": "0",
+                    "max_learning_rate": "0.135",
+                    "min_learning_rate": "0.135",
+                    "target_block_utilization": "5000",
+                    "max_block_utilization": "30000000",
+                    "window": "1",
+                    "enabled": true,
+                    "default_fee_denom": "uluna",
                 });
-                result = await LCD.chain1.tx.broadcastSync(sendTx, "test-1");
-                await blockInclusion();
-            }
+        }
+        catch (e: any) {
+            console.log(e)
+            expect(e).toBeUndefined();
+        }
+    });
 
-            const minGasPrice = BigNumber("0.0015");
+    test('Must send a transaction and validate the fee', async () => {
+        try {
+            // Send a transaction to test the dynamic fees
+            let sendTx = await val1Wallet.createAndSignTx({
+                msgs: [
+                    new MsgSend(
+                        val1WalletAddress,
+                        new MnemonicKey().accAddress("terra"), // To a random account
+                        Coins.fromString("1uluna"),
+                    ),
+                ],
+                chainID: "test-1",
+            });
+            const result = await LCD.chain1.tx.broadcastSync(sendTx, "test-1");
+            await blockInclusion();
+            const txResult = await LCD.chain1.tx.txInfo(result.txhash, "test-1")
+            expect(txResult.code).toBe(0);
+
             let congested = true;
             let counter = 0;
-            for (let i = 0; i < 100; i++) {
-                const gasPrice = await getGasPrice("test-1", "uluna")
+            while (true) {
+                const res = (await LCD.chain1.feemarket.feeDenomParam("test-1", "uluna"))[0];
+                let gasPrice = res.baseFee;
+                let minBaseFee = res.minBaseFee;
                 if (congested) {
-                    if (gasPrice.isEqualTo(minGasPrice)) {
+                    if (gasPrice.equals(minBaseFee)) {
                         congested = false;
                     } else {
-                        expect(gasPrice.isGreaterThan(minGasPrice)).toBe(true);
+                        expect(gasPrice.greaterThan(minBaseFee)).toBe(true);
                         console.log(`congested gasPrice: ${gasPrice.toString()}`)
                     }
                 } else {
-                    if (counter > 5) break;
-                    if (gasPrice.isGreaterThan(minGasPrice)) {
+                    if (counter > 3) break;
+                    if (gasPrice.greaterThan(minBaseFee)) {
                         congested = true;
                         counter = 0;
                     } else {
-                        expect(gasPrice.eq(minGasPrice)).toBe(true);
+                        expect(gasPrice.eq(minBaseFee)).toBe(true);
                         counter++;
                         console.log(`non-congested gasPrice: ${gasPrice.toString()} counter: ${counter}`)
                     }
                 }
-                // wait for 1 sec
-                await new Promise(resolve => setTimeout(resolve, 1000));
+                // wait for a new block
+                await blockInclusion();
             }
         }
         catch (e: any) {
-            expect(e).toBeFalsy();
+            console.log(e)
+            expect(e).toBeUndefined();
         }
     });
 
-    const getGasPrice = async (chainId: string, feeDenom: string): Promise<BigNumber> => {
-        const foundFdp = await LCD.chain1.feemarket.feeDenomParam(chainId, feeDenom) as  any;
-        const fdp = foundFdp.fee_denom_params[0] as any;
-        const gasPrice = BigNumber(fdp.base_fee)
-        return gasPrice
-    }
-
-    test('Must update feedenomparam for uluna', async () => {
+    test('Must update fee denom param for uluna', async () => {
         try {
-            const msgProposal = new MsgSubmitProposal(
-                [new MsgFeeDenomParam(
-                    'uluna',
-                    '1550000000000000',
-                    'terra10d07y265gmmuvt4z0w9aw880jnsr700juxf95n',
-                    )],
-                Coins.fromString("1000000000uluna"),
-                val1WalletAddress,
-                "metadata",
-                "title",
-                "summary"
-            );
             // Create an state update proposal sign and submit on chain-1
             let tx = await val1Wallet.createAndSignTx({
-                msgs: [msgProposal],
+                msgs: [new MsgSubmitProposal(
+                    [new MsgFeeDenomParam(
+                        'uluna',
+                        '1550000000000000',
+                        'terra10d07y265gmmuvt4z0w9aw880jnsr700juxf95n',
+                    )],
+                    Coins.fromString("1000000000uluna"),
+                    val1WalletAddress,
+                    "metadata",
+                    "title",
+                    "summary"
+                )],
                 chainID: "test-1",
+                fee: new Fee(1000000, Coins.fromString("1000000uluna"))
             });
             let result = await LCD.chain1.tx.broadcastSync(tx, "test-1");
-
             await blockInclusion();
 
             // Check that the proposal was created successfully
             let txResult = await LCD.chain1.tx.txInfo(result.txhash, "test-1") as any;
-
             expect(txResult.code).toBe(0);
 
             // Get the proposal id and validate exists
-            let proposalId = Number(txResult.logs[0].eventsByType.submit_proposal.proposal_id[0]);
-
-            expect(proposalId)
+            const proposalId = Number(txResult.logs[0].eventsByType.submit_proposal.proposal_id[0]);
+            expect(proposalId).toBeDefined();
 
             // Vote for the proposal
             tx = await val1Wallet.createAndSignTx({
@@ -171,32 +191,34 @@ describe("Feemarket Module (https://github.com/terra-money/feemarket/tree/v0.0.1
                     VoteOption.VOTE_OPTION_YES
                 )],
                 chainID: "test-1",
+                fee: new Fee(1000000, Coins.fromString("1000000uluna"))
             });
             result = await LCD.chain1.tx.broadcastSync(tx, "test-1");
             await votingPeriod();
+
+            // Validate the tx vote was casted successflully
             txResult = await LCD.chain1.tx.txInfo(result.txhash, "test-1")
             expect(txResult.code).toBe(0);
 
             // Query the feemarket state for uluna and validate the new values
-            let foundFdp = await LCD.chain1.feemarket.feeDenomParam("test-1", "uluna") as any;
-            expect(foundFdp.fee_denom_params[0].min_base_fee).toEqual("0.001550000000000000")
+            const res = (await LCD.chain1.feemarket.feeDenomParam("test-1", "uluna"))[0];
+            expect(res.feeDenom).toEqual("uluna");
+            expect(res.baseFee.toNumber()).toBeGreaterThan(0.00155);
+            expect(res.minBaseFee.toString()).toStrictEqual("0.00155");
         }
         catch (e: any) {
-            expect(e).toBeFalsy();
+            console.log(e)
+            expect(e).toBeUndefined();
         }
     });
-});
 
-const checkParams = (foundParams: any, params: Params) => {
-    const exponent = BigNumber(10).exponentiatedBy(18);
-    expect(BigNumber(foundParams.alpha).multipliedBy(exponent).isEqualTo(BigNumber(params.alpha))).toBe(true);
-    expect(BigNumber(foundParams.beta).multipliedBy(exponent).isEqualTo(BigNumber(params.beta))).toBe(true);
-    expect(BigNumber(foundParams.theta).multipliedBy(exponent).isEqualTo(BigNumber(params.theta))).toBe(true);
-    expect(BigNumber(foundParams.min_learning_rate).multipliedBy(exponent).isEqualTo(BigNumber(params.minLearningRate))).toBe(true);
-    expect(BigNumber(foundParams.max_learning_rate).multipliedBy(exponent).isEqualTo(BigNumber(params.maxLearningRate))).toBe(true);
-    expect(foundParams.target_block_utilization).toBe(params.targetBlockUtilization);
-    expect(foundParams.max_block_utilization).toBe(params.maxBlockUtilization);
-    expect(foundParams.window).toBe(params.window);
-    expect(foundParams.enabled).toBe(params.enabled);
-    expect(foundParams.default_fee_denom).toBe(params.defaultFeeDenom);
-}
+    // After all the tests are executed, 
+    // start the relayer again
+    afterAll(() => {
+        // Create the path
+        const pathToRelayDir = path.join(__dirname, "/../../test-data/relayer");
+        // Start the relayer again
+        const relayerStart = exec(`relayer start "test1-test2" -p="events" -b=100 --flush-interval="1s" --time-threshold="1s" --home="${pathToRelayDir}" > ${pathToRelayDir}/relayer.log 2>&1`)
+        relayerStart.unref();
+    });
+});
