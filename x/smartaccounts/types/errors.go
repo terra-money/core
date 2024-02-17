51510f699992c90e35c8a2efc6df91aa959e6594
freeelancer <lmengsplist96@gmail.com>
2024-02-17 22:30:54 +0800

    added setting proto

--- x/smartaccounts/types/errors.go
+++ x/smartaccounts/types/errors.go
@@ -5,10 +5,5 @@ import (
 )
 
 var (
-	ErrFeeShareDisabled              = errorsmod.Register(ModuleName, 1, "smartaccounts module is disabled by governance")
-	ErrFeeShareAlreadyRegistered     = errorsmod.Register(ModuleName, 2, "smartaccounts already exists for given contract")
-	ErrFeeShareNoContractDeployed    = errorsmod.Register(ModuleName, 3, "no contract deployed")
-	ErrFeeShareContractNotRegistered = errorsmod.Register(ModuleName, 4, "no smartaccounts registered for contract")
-	ErrFeeSharePayment               = errorsmod.Register(ModuleName, 5, "smartaccounts payment error")
-	ErrFeeShareInvalidWithdrawer     = errorsmod.Register(ModuleName, 6, "invalid withdrawer address")
+	ErrFeeShareDisabled = errorsmod.Register(ModuleName, 1, "smartaccounts module is disabled by governance")
 )
