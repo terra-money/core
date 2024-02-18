deb83ca84944cdc3f82cc22da1a10f2a9759d3a4
freeelancer <lmengsplist96@gmail.com>
2024-02-18 12:42:43 +0800

    created msg skeletons

--- x/smartaccount/keeper/msg_server.go
+++ x/smartaccount/keeper/msg_server.go
@@ -1,7 +1,51 @@
 package keeper
 
 import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/terra-money/core/v2/x/smartaccount/types"
 )
 
-var _ types.MsgServer = &Keeper{}
+type MsgServer struct {
+	k Keeper
+}
+
+// NewMsgServer returns the MsgServer implementation.
+func NewMsgServer(k Keeper) types.MsgServer {
+	return &MsgServer{k}
+}
+
+func (ms MsgServer) CreateSmartAccount(goCtx context.Context, msg *types.MsgCreateSmartAccount) (*types.MsgCreateSmartAccountResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	if err := ms.k.SetSetting(ctx, msg.Account, types.Setting{
+		Owner: msg.Account,
+	}); err != nil {
+		return nil, err
+	}
+	return &types.MsgCreateSmartAccountResponse{}, nil
+}
+
+func (ms MsgServer) Authorization(goCtx context.Context, msg *types.MsgAuthorization) (*types.MsgAuthorizationResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	_ = ctx
+	return &types.MsgAuthorizationResponse{}, nil
+}
+
+func (ms MsgServer) UpdateAuthorization(goCtx context.Context, msg *types.MsgUpdateAuthorization) (*types.MsgUpdateAuthorizationResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	_ = ctx
+	return &types.MsgUpdateAuthorizationResponse{}, nil
+}
+
+func (ms MsgServer) UpdateTransactionHooks(goCtx context.Context, msg *types.MsgUpdateTransactionHooks) (*types.MsgUpdateTransactionHooksResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	_ = ctx
+	return &types.MsgUpdateTransactionHooksResponse{}, nil
+}
+
+func (ms MsgServer) DisableSmartAccount(goCtx context.Context, msg *types.MsgDisableSmartAccount) (*types.MsgDisableSmartAccountResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	_ = ctx
+	return &types.MsgDisableSmartAccountResponse{}, nil
+}
--- x/smartaccount/module.go
+++ x/smartaccount/module.go
@@ -145,7 +145,7 @@ func (am AppModule) QuerierRoute() string {
 // RegisterServices registers a GRPC query service to respond to the
 // module-specific GRPC queries.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
-	types.RegisterMsgServer(cfg.MsgServer(), am.keeper)
+	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServer(am.keeper))
 	types.RegisterQueryServer(cfg.QueryServer(), keeper.NewQuerier(am.keeper))
 }
 
