b65a038718eb61a731707d11d76aa92d1b20e125
javiersuweijie <javier.su.weijie@gmail.com>
2024-02-22 11:48:54 +0800

    test: added sample auth contract wasm for testing

--- x/smartaccount/test_helpers/test_data.go
+++ x/smartaccount/test_helpers/test_data.go
@@ -4,3 +4,6 @@ import _ "embed"
 
 //go:embed test_data/limit_send_only_hooks.wasm
 var LimitSendOnlyHookWasm []byte
+
+//go:embed test_data/smart_auth_contract.wasm
+var SmartAuthContractWasm []byte
