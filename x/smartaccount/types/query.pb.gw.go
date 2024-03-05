1ef05b7b27267835267eeb707c9a1ac2eb42fb8e
freeelancer <lmengsplist96@gmail.com>
2024-03-05 14:19:50 +0800

    update setting api url

--- x/smartaccount/types/query.pb.gw.go
+++ x/smartaccount/types/query.pb.gw.go
@@ -244,7 +244,7 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 var (
 	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"terra", "smartaccount", "v1", "params"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_Setting_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"terra", "smartaccount", "v1", "params", "address"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_Setting_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"terra", "smartaccount", "v1", "setting", "address"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
 var (
